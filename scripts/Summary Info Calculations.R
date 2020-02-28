f249727786d69c2a494230b08ccaed81e71e3802
emmaschnee <emmas32@uw.edu>
2020-02-28 15:18:02 -0800

    work on summary calculations

--- scripts/Summary Info Calculations.R
+++ scripts/Summary Info Calculations.R
@@ -2,6 +2,7 @@
 library("dplyr")
 library("ggplot2")
 library("stringr")
+rm(list = ls())
 #Load data set
 
 food_price_data <- read.csv("data/wfp_market_food_prices.csv",
@@ -14,7 +15,9 @@ prev_undernourished_final_data <- select(prevelence_undernourishment_data, GeoAr
   rename(
     location_name = GeoAreaName, 
     year = TimePeriod, 
-    value = Value)
+    value = Value) 
+
+prev_undernourished_final_data <- lapply(prev_undernourished_final_data, gsub, pattern = '""', replacement = '' )
 
 food_price_data_final <- select(food_price_data, adm0_name, cm_name, cur_name, mp_year, mp_price) %>%
   rename(
@@ -24,24 +27,29 @@ food_price_data_final <- select(food_price_data, adm0_name, cm_name, cur_name, m
     year = mp_year,
     price = mp_price
   )
-#Creat summary info function
-get_summary_info <- function(prevelence_undernourishment_data) {
-  ret <- list(
-    num_rows_2016 = nrow(prevelence_undernourishment_data$TimePeriod = 2016),
-    df_uniq_countries = 
-    
-  ) 
-}
-#get_summary_info <- function(food_price_data) {
-  #ret <- list(
-   # num_rows_2016 = nrow(food_price_data$mp_year = 2016),
-    #df_uniq_countries = nrow(distinct(food_price_data$adm0_name))
-    
-  #) 
-#}
 
-num_countries_2017 <- select(prevelence_undernourishment_data, 
-                             GeoAreaName, TimePeriod, Value) %>%
-  + filter(TimePeriod == ""2017"") %>% 
-  + group_by(GeoAreaName) %>%
-  + summarize(num_countries = length(GeoAreaName))
+#Creat summary info function
+get_summary_info <- function(prev_undernourished_final_data) {
+  ret <- list(
+    recent_year = select(prev_undernourished_final_data, year) %>%
+      group_by(year) %>%
+      summarize(recent_year = max(year)) %>%
+      filter(recent_year == max(recent_year, na.rm = TRUE)) %>%
+      pull(year),
+    earliest_year = select(prev_undernourished_final_data, year) %>%
+      group_by(year) %>%
+      summarize(first_year = min(year)) %>%
+      filter(first_year == min(first_year, na.rm = TRUE)) %>%
+      pull(year),
+    country_most_undernourished = select(prev_undernourished_final_data, location_name, value) %>%
+      group_by(location_name) %>%
+      summarize(value = max(value, na.rm = TRUE)) %>%
+      filter(value == max(value, na.rm = TRUE)) %>%
+      pull(location_name)
+  ) 
+  return(ret)
+}
+
+#num_location = select(prev_undernourished_final_data, location_name) %>%
+ # group_by(location_name) %>%
+  #summarize(num_locations = length(location_name)) %>%
