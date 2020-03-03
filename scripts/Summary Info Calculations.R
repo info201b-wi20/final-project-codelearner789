6b14f91d56e01fac9cdeee9347f9f9cf992f9a55
emmaschnee <emmas32@uw.edu>
2020-03-02 17:50:59 -0800

    took out world data

--- scripts/Summary Info Calculations.R
+++ scripts/Summary Info Calculations.R
@@ -7,22 +7,20 @@ rm(list = ls())
 
 #organize data & Rename
 
-
-
 prev_undernourished_final_data <- select(prevelence_undernourishment_data, GeoAreaName, 
-                                         TimePeriod, Value, Goal) %>%
-
+                                         TimePeriod, Value) %>%
   rename(
     location_name = GeoAreaName, 
     year = TimePeriod, 
-    value = Value,
-    goal = Goal) 
+    value = Value)
 
+prev_undernourished_final_data <- filter(prev_undernourished_final_data, location_name != "World")
 prev_undernourished_final_data <- as.data.frame(lapply(prev_undernourished_final_data,
                                                        function(x) gsub('\"', '', x)))
 prev_undernourished_final_data$year <- as.numeric(as.character(prev_undernourished_final_data$year))
 prev_undernourished_final_data$value <- as.numeric(as.character(prev_undernourished_final_data$value))
 prev_undernourished_final_data <- prev_undernourished_final_data[complete.cases(prev_undernourished_final_data), ]
+
 food_price_data_final <- select(food_price_data, adm0_name, cm_name, cur_name, mp_year, mp_price, um_id, um_name) %>%
   rename(
     country_name = adm0_name,
@@ -47,7 +45,7 @@ get_summary_info <- function(prev_undernourished_final_data) {
       summarize(first_year = min(year)) %>%
       filter(first_year == min(first_year, na.rm = TRUE)) %>%
       pull(year),
-    country_most_undernourished <- select(prev_undernourished_final_data, location_name, value) %>%
+    country_most_undernourished = select(prev_undernourished_final_data, location_name, value) %>%
       filter(value == max(value, na.rm = TRUE)) %>%
       pull(location_name),
     most_num_people_undernourished = select(prev_undernourished_final_data, value) %>%
