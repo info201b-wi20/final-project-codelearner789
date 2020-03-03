57fbea6778e712dadbc6a6ee55768c8b4f9805a2
Mo <motilmo14@gmail.com>
2020-03-02 16:56:10 -0800

    Chart 2

--- scripts/Function for second chart.R
+++ scripts/Function for second chart.R
@@ -4,17 +4,23 @@ library("ggplot2")
 library("leaflet")
 
 
-all_entries <- prev_undernourished_final_data %>%
-  
-  group_by(commodity_purch, year) %>% 
-  summarize(price=mean(price))
+malnourishment_Zimbabwe <- prev_undernourished_final_data %>%
+  filter(location_name == "Zimbabwe") %>%
+  filter(value == max(value)) %>%
+  pull(year)
+
+all_entries <- food_price_data_final %>%
+  filter(year == malnourishment_Zimbabwe) %>%
+  filter(country_name == "Zimbabwe") %>%
+  group_by(commodity_purch) %>% 
+  summarize(price= max(price))
 
 second_chart <- function(df) {
   plot_2 <- ggplot(data = df) +
-    geom_line(mapping = aes(x = year,
-                            y = price,
-                            color = commodity_purch))
-  return(plot_2)
+    geom_bar(mapping = aes(x = commodity_purch, y = price), stat = "identity")
+  
+return(plot_2)
 }
+
 plot_2 <- second_chart(all_entries)
  
