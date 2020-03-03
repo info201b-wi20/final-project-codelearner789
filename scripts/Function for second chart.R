4f19d8717d6a2dd81ea0d8b0fe1abdb5fff88154
emmaschnee <emmas32@uw.edu>
2020-03-02 18:59:12 -0800

    small edits to chart 2

--- scripts/Function for second chart.R
+++ scripts/Function for second chart.R
@@ -20,12 +20,12 @@ second_chart <- function(df) {
     geom_bar(mapping = aes(x = commodity_purch, y = price), stat = "identity") +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
     labs(
-      title = "Average price of each commodity type",
-      x = "Commodity",
-      y = "Average Price"
+      title = "Average price of each commodity type in Zimbabwe 2015",
+      x = "Commodity (5kg)",
+      y = "Average Price USD"
     )
 return(plot_2)
 }
 
 plot_2 <- second_chart(all_entries)
- 
+plot_2
