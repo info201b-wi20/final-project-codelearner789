d1827ea80bad62401e9d2013d94e7e3ba61bc3e2
emmaschnee <emmas32@uw.edu>
2020-03-09 14:27:45 -0700

    changes to vis3

--- server_3.R
+++ server_3.R
@@ -4,5 +4,18 @@ library("ggplot2")
 library("plotly")
 
 server3 <- function(input, output) {
-  
+  output$vis1 <- renderPlot({
+    df <- prev_undernourished_final_data
+    title <- paste0("Coutry Undernourishment Rates (", 
+                    input$slider_value, ") 2000 - 2017")
+    vis_1 <- ggplot(data = df, aes(x = year)) +
+      geom_line(mapping = aes(y = input$slider_value, col = location_name)) +
+      labs(title = title, 
+           x = "Year 2000-2017",
+           y = paste0("Undernourishment Values: ",
+                      input$slider_value))
+    vis_1
+  })
 }
+
+
--- vis_3.R
+++ vis_3.R
@@ -1,4 +1,58 @@
+# load data
+
+prevelence_undernourishment_data <-
+  read.csv("data/prevelence_undernourished.csv",
+           stringsAsFactors = FALSE
+  )
+
+colnames(prevelence_undernourishment_data)[colnames(prevelence_undernourishment_data) == "ï..Goal"] <- "Goal"
+
+prev_undernourished_final_data <- select(
+  prevelence_undernourishment_data, GeoAreaName,
+  TimePeriod, Value) %>%
+  rename(
+    location_name = GeoAreaName,
+    year = TimePeriod,
+    value = Value
+  )
+
+prev_undernourished_final_data <- filter(prev_undernourished_final_data,
+                                         location_name != "World")
+prev_undernourished_final_data <- as.data.frame(lapply(
+  prev_undernourished_final_data,
+  function(x) gsub('\"', "", x)
+))
+prev_undernourished_final_data$year <- as.numeric(
+  as.character(prev_undernourished_final_data$year))
+prev_undernourished_final_data$value <- as.numeric(
+  as.character(prev_undernourished_final_data$value))
+prev_undernourished_final_data <- prev_undernourished_final_data[complete.cases(
+  prev_undernourished_final_data), ]
+
+#Create Variables & Visualization for Page 3
+var1_vis3 <- sidebarLayout(
+  sidebarPanel(
+    sliderInput("slider_value",
+                label = h3("Choose a Range of Values"),
+                min = 2,  max = 75, value = c(40, 50))
+    ),
+  mainPanel(
+    h3("Find Race Percentages by State"),
+    plotOutput("vis1", click = "plot_click1"),
+    verbatimTextOutput("info1"),
+    verbatimTextOutput("range")
+  )
+)
+
 # Visualization 3
 vis_3 <- tabPanel(
-  "Third Visualization"
+  "Undernourishment Rates",
+  titlePanel("Find Which Countries Have Similar Undernourishment Rates"),
+  p("In this page we are trying to answer the question of 
+  which countries are similar in the amount of 
+  undernourishment their populations are facing. 
+  You can use the graphics the select a range of values
+    and see where countries fall within those 
+    values of undernourishment."),
+  var1_vis3
 )
