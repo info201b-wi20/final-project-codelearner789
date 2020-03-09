56941be1865c2f5559cbc8e3e1b78e2fc511de00
codelearner789 <brookchen943@yahoo.com>
2020-03-09 10:01:18 -0700

    Final Shiny App

--- app_ui.R
+++ app_ui.R
@@ -1,5 +1,23 @@
+library("shiny")
+library("dplyr")
+library("ggplot2")
+library("plotly")
+# Introduction
+intro_tab <- tabPanel(
+  "Introduction"
+  
+)
 
+# Visualization 1
 
+# Visualization 2
+
+# Visualization 3
+
+# Conclusion
+concluding_tab <- tabPanel(
+  "Ending Remarks"
+)
 
 ui <- navBarPage(
   "Exploring food accessibility an malnourishment"
