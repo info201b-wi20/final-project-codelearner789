32c78bbd0e5fa2cded3dc15d5bfa8ef66af70a36
codelearner789 <brookchen943@yahoo.com>
2020-03-09 10:03:45 -0700

    Shiny UI

--- app_ui.R
+++ app_ui.R
@@ -9,17 +9,28 @@ intro_tab <- tabPanel(
 )
 
 # Visualization 1
-
+vis_1 <- tabPanel(
+  "First Visualization"
+)
 # Visualization 2
-
+vis_2 <- tabPanel(
+  "Second Visualization"
+)
 # Visualization 3
-
+vis_3 <- tabPanel(
+  "Third Visualization"
+)
 # Conclusion
 concluding_tab <- tabPanel(
   "Ending Remarks"
 )
 
 ui <- navBarPage(
-  "Exploring food accessibility an malnourishment"
+  "Exploring food accessibility an malnourishment",
+  intro_tab,
+  vis_1,
+  vis_2,
+  vis_3,
+  concluding_tab
   
 )
