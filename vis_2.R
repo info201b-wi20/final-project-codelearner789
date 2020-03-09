4a63fd1e978a28f92e7da00c9edbbf5bbb829a88
Mo <motilmo14@gmail.com>
2020-03-09 14:51:51 -0700

    Widgets for vis2

--- vis_2.R
+++ vis_2.R
@@ -3,7 +3,43 @@ library("dplyr")
 library("ggplot2")
 library("plotly")
 
+prevelence_undernourishment_data <-
+  read.csv("data/prevelence_undernourished.csv",
+           stringsAsFactors = FALSE)
+food_price_data <- read.csv("data/wfp_market_food_prices.csv",
+                            stringsAsFactors = FALSE)
+
+
+all_countries <- food_price_data %>%
+  group_by(adm0_name) %>%
+  summarize(num_countries = n()) %>%
+  pull(adm0_name)
+
 # Visualization 2
 vis_2 <- tabPanel(
-  "Second Visualization"
+  "Second Visualization",
+  choose_commodity,
+  choose_country
+)
+choose_commodity <- sidebarLayout(
+  selectInput(
+    inputId = "commodity",
+    label = "Choose a commodity",
+    choices = list("Bread" = "Bread", "Wheat" = "Wheat", 
+                   "Rice" = "Rice","Maize" = "Maize", 
+                   "Sorghum" = "Sorghum",  "Millet" = "Millet")
+    
+  ),
+  mainPanel(
+    "Change commodity",
+    plotOutput("")
+  ),
+)
+choose_country <- sidebarLayout(
+  selectInput(
+    inputId = "Country",
+    label = "Choose a country",
+    choices = all_countries,
+    selected = all_countries[1],
+  )
 )
