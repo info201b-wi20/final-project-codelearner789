f920db3517059304c59867e0398c490669a4cbe3
codelearner789 <brookchen943@yahoo.com>
2020-03-09 18:39:44 -0700

    Second Visualization

--- server_2.R
+++ server_2.R
@@ -3,6 +3,52 @@ library("dplyr")
 library("ggplot2")
 library("plotly")
 
+prevelence_undernourishment_data <-
+  read.csv("data/prevelence_undernourished.csv",
+           stringsAsFactors = FALSE)
+food_price_data <- read.csv("data/wfp_market_food_prices.csv",
+                            stringsAsFactors = FALSE)
+colnames(prevelence_undernourishment_data)[colnames(prevelence_undernourishment_data) == "ï..Goal"] <- "Goal"
+
+food_price_data_final <- select(food_price_data, adm0_name,
+                                cm_name, cur_name, mp_year, mp_price,
+                                um_id, um_name) %>%
+  rename(
+    country_name = adm0_name,
+    commodity_purch = cm_name,
+    currency = cur_name,
+    year = mp_year,
+    price = mp_price,
+    unit = um_id,
+    unit_type = um_name)
+
 server2 <- function(input, output) {
-  
+  output$plot_no2 <- renderPlot({
+    
+    df <- food_price_data_final %>%
+      filter(year == input$year) %>%
+      filter(country_name == input$Country) %>%
+      group_by(commodity_purch) %>%
+      summarise(price = max(price))
+    currency_used <- food_price_data_final %>%
+      filter(country_name == input$Country) %>%
+      group_by(currency) %>%
+      summarize(num = n()) %>%
+      pull(currency)
+    titleText <- paste0("Average Price in ", currency_used)
+    plot <- ggplot(data = df) +
+      geom_col(mapping = aes_string(
+        x = "commodity_purch",
+        y = "price"
+      )) +
+      theme(axis.text.x = element_text(
+        angle = 90, hjust = 1
+      )) +
+      labs(
+        title = titleText,
+        x = "Commodity Type",
+        y = "Average Price"
+      )
+    return(plot)
+  })
 }
--- vis_2.R
+++ vis_2.R
@@ -15,22 +15,37 @@ all_countries <- food_price_data %>%
   summarize(num_countries = n()) %>%
   pull(adm0_name)
 
+food_price_data_final <- select(food_price_data, adm0_name,
+                                cm_name, cur_name, mp_year, mp_price,
+                                um_id, um_name) %>%
+  rename(
+    country_name = adm0_name,
+    commodity_purch = cm_name,
+    currency = cur_name,
+    year = mp_year,
+    price = mp_price,
+    unit = um_id,
+    unit_type = um_name)
+
+all_years <- food_price_data_final %>%
+  group_by(year) %>%
+  summarise(num_recorded = n()) %>%
+  pull(year)
 # Visualization 2
 
 choose_commodity <- sidebarLayout(
   sidebarPanel(
     selectInput(
-      inputId = "commodity",
-      label = "Choose a commodity",
-      choices = list("Bread" = "Bread", "Wheat" = "Wheat", 
-                     "Rice" = "Rice","Maize" = "Maize", 
-                     "Sorghum" = "Sorghum",  "Millet" = "Millet")
-      ),
-    selectInput(
       inputId = "Country",
       label = "Choose a country",
       choices = all_countries,
       selected = all_countries[1],
+    ),
+    selectInput(
+      inputId = "year",
+      label = "Pick year",
+      choices = all_years,
+      selected = all_years[22]
     )
   ),
   mainPanel(
