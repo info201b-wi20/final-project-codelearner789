90a85c5ea4bd5d7610eef59b463c405026e7f249
mck27 <mck27@uw.edu>
2020-03-10 18:45:40 -0700

    Madeline edit

--- vis_1.R
+++ vis_1.R
@@ -1,4 +1,15 @@
 # Visualization 1
+library("shiny")
+library("dplyr")
+library("ggplot2")
+library("plotly")
+prevelence_undernourishment_data <-
+  read.csv("data/prevelence_undernourished.csv",
+           stringsAsFactors = FALSE)
+
+
+
+
 vis_1 <- tabPanel(
   "First Visualization"
 )
