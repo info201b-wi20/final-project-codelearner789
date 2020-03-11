6f27d1618cf37e0e0bcba2a0745998b8ec3e398f
mck27 <mck27@uw.edu>
2020-03-10 21:50:15 -0700

    madeline edits

--- server_1.R
+++ server_1.R
@@ -4,5 +4,19 @@ library("ggplot2")
 library("plotly")
 
 server1 <- function(input, output) {
-
+  output$vis1 <- renderPlotly({
+    df1 <- prev_final %>%
+      select(location_name, year, value) %>%
+      filter(location_name %in% c("Zimbabwe", "Honduras", 
+                                  "Timor-Leste", "Costa Rica", "El Salvador",
+                                  "Honduras", "Panama")) %>%
+      filter(location_name == input$country1)
+    vis_1 <- ggplot(data = df1) +
+      geom_point(mapping = aes(
+        x = "year",
+        y = "value",
+        color = "location_name")) +
+      ggtitle("Test")
+    vis_1
+  })
 }
