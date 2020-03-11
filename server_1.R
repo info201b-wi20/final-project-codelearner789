90fbdba0593a7c0132256db76699c2d49759e55e
codelearner789 <brookchen943@yahoo.com>
2020-03-10 18:06:12 -0700

    Final Project

--- server_1.R
+++ server_1.R
@@ -4,20 +4,5 @@ library("ggplot2")
 library("plotly")
 
 server1 <- function(input, output) {
-  one_country <- prev_undernourished_final_data %>%
-    filter(location_name == "Zimbabwe") %>%
-    group_by(year)%>%
-    summarize(values_in_percents = mean(value))
-  
-  first_chart <- function(df) {
-    plot <- ggplot(data = df) +
-      geom_point(mapping = aes(x = year,
-                               y = values_in_percents,
-                               color = "Zimbabwe",
-      )) +
-      ggtitle("% of Population Undernourished in Zimbabwe")
-    return(plot)
-  }
-  plot_1 <- first_chart(one_country)
-  
+
 }
